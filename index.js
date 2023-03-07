59d5608f3f8cc57c3fc3e66cceb5526cb2f4e55d
radio server <radioserver98@gmail.com>
2023-03-07 20:23:04 -0300

    update local

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 app.all('/', (req, res) => {
     console.log("Just got a request!")
-    res.send('Yo!')
+    res.send('http://192.168.0.202:3000')
 })
 app.listen(process.env.PORT || 3000)
