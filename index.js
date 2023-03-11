65ac0a00bfd8871b8f99e7dfe205597468ab82a2
radio server <116530094+rserver98@users.noreply.github.com>
2023-03-11 00:19:38 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 app.all('/', (req, res) => {
     console.log("Just got a request!")
-    res.send('http://192.168.0.202:3000')
+    res.send('https://0b4f-190-192-75-210.sa.ngrok.io')
 })
 app.listen(process.env.PORT || 3000)
