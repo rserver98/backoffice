1aaeca54cdffc29688e8c88eed4092c33c67aefd
radio server <radioserver98@gmail.com>
2023-03-11 12:51:53 -0300

    express limit

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express');
+var bodyParser = require('body-parser')
 const app = express();
 const PORT = process.env.PORT || 4500;
 
+app.use(bodyParser.json({ limit: '50mb' }))
+
 app.all('/ok', (req, res) => {
-    res.send('https://0b4f-190-192-75-210.sa.ngrok.io')
+    const date = new Date();
+    const day = `${date.getDate()}/${date.getMonth()+1}/${date.getFullYear()}`
+    const time = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
+    res.send(`OK! ${day} ${time}`)
 })
 
 app.all('/url', (req, res) => {
